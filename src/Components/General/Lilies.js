ec1e9cbba68b4f856aab7beccbd6599c894b0df9
MubarakMKay <mubi4lyf@gmail.com>
2022-09-30 10:54:12 +0100

    modified:   src/Components/General/Lilies.js 	modified:   src/Styles/Dashboard.css 	modified:   src/Styles/LandingPage.css

--- src/Components/General/Lilies.js
+++ src/Components/General/Lilies.js
@@ -1,11 +1,14 @@
+import { Link } from 'react-router-dom';
 import LiliesIcon from '../../Assets/LiliesIcon.png';
 
 const Lilies = () => {
     return ( 
-        <div className="lilies">
-            <img src={LiliesIcon} alt="Lilies" />
-            <h1 className="liliesText">Lilies</h1>
-        </div>
+        <Link to='/' className='dashboardLink'>
+            <div className="lilies">
+                <img src={LiliesIcon} alt="Lilies" />
+                <h1 className="liliesText">Lilies</h1>
+            </div>
+        </Link>
      );
 }
  
