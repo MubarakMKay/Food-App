c0bb3a0decaeed1a379ccc63a9db5bde8f362688
MubarakMKay <mubi4lyf@gmail.com>
2022-10-01 10:40:39 +0100

    modified:   src/Components/Login/LoginMain.js 	modified:   src/Styles/Login.css

--- src/Components/Login/LoginMain.js
+++ src/Components/Login/LoginMain.js
@@ -12,7 +12,7 @@ const LoginMain = () => {
                     <form>
                         <input type="email" name="Email" id="" placeholder='Your Email Address' required/>
                         <input type="password" name="Password" id="" placeholder='Your Password' required/>
-                        <input type="submit" value="LOGIN" />
+                        <Link to='/dashboard' className='dashboardLink'><input type="submit" value="LOGIN" /></Link>
                     </form>
                     <div className="loginDetailsOthers">
                         <span><Link to="/signup" className='loginLink'>Create an account</Link></span>
